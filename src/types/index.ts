c632e9255a588a2b6ede30a60c12e3738fd446ea
Mickael Rebeau <rebeau.mickael@gmail.com>
2025-01-16 00:33:07 +0100

    feat: define Quiz and Question data types for quiz functionality

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+export type QuizDatatype = {
+  questions: QuestionItem[];
+  difficulty: string;
+  numberOfQuestions: number;
+  topic: string;
+};
+
+export type QuestionItem = {
+  question: string;
+  correctAnswer: string;
+  incorrectAnswers: string[];
+  explanation: string;
+  shuffledAnswers?: string[];
+};
+
+export type UserAnswerType = {
+  question: string;
+  correctAnswer: string;
+  userAnswer: string;
+};
+
+export type userQuizType = {
+  topic: string | undefined;
+  score: number | null;
+  userAnswers: UserAnswerType[];
+};
