241434cdea93b090581a5351913cc8b43ed373e4
Mickael Rebeau <rebeau.mickael@gmail.com>
2025-02-06 20:44:24 +0100

    refactor: remove Lottie animation

--- src/app/[...messing].tsx
+++ src/app/[...messing].tsx
@@ -1,28 +1,16 @@
 import { Stack } from 'expo-router';
-import LottieView from 'lottie-react-native';
 import * as React from 'react';
 
-import { View } from '@/components/ui';
+import { Text,View } from '@/components/ui';
 
 export default function NotFoundScreen() {
-  const animation = React.useRef<LottieView>(null);
-  
   return (
     <>
       <Stack.Screen options={{ title: "Oops! This screen doesn't exist." }} />
       <View className="flex size-full items-center justify-center">
-        <LottieView
-          autoPlay
-          loop
-          ref={animation}
-          style={{
-            width: 300,
-            height: 300,
-            backgroundColor: '#eee',
-          }}
-          // Find more Lottie files at https://lottiefiles.com/featured
-          source={require('assets/404.json')}
-        />
+        <Text className="text-center">
+          This screen doesn't exist. Please check the URL and try again.
+        </Text>
       </View>
     </>
   );
